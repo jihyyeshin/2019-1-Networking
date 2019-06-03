e709cbedaa6376393bcbd48029281431d3ec6f57
jihyyeshin <tlsjh082@gmail.com>
2019-06-04 02:34:52 +0900

    190604 plain code + option 1 complete

--- ICMP_Pinger/icmp_pinger.py
+++ ICMP_Pinger/icmp_pinger.py
@@ -33,7 +33,8 @@ def checksum(string):
 
 # ping 받기, socket, id, timeout, destaddr
 def receiveOnePing(mySocket, ID, timeout, destAddr):
-    #global rtt_min, rtt_max, rtt_sum, rtt_cnt
+    global rttMin, rttMax, rttSum, rttCnt
+
     timeLeft = timeout# timeout 예외
     while 1:
         startedSelect = time.time()#현재 
@@ -50,26 +51,24 @@ def receiveOnePing(mySocket, ID, timeout, destAddr):
         icmpheader=recPacket[20:28]
         type, code, checksum, pid, seq = struct.unpack('bbHHh', icmpheader)
 
-        # if type != 0:
-        #     return 'expected type=0, but got {}'.format(type)
-        # if code != 0:
-        #     return 'expected code=0, but got {}'.format(code)
-        # if ID != pid:
-        #     return 'expected pid={}, but got {}'.format(ID, pid)
+        if type==8 or ID!=pid:#wrong type
+        	return 'Wrong Type.'
+        
         send_time,  = struct.unpack('d', recPacket[28:])# 보낸 시간 패킷에 가서 가져온다. 
         
+        #OPTION 1
         rtt = (timeReceived - send_time) * 1000
-        # rtt_cnt += 1
-        # rtt_sum += rtt
-        # rtt_min = min(rtt_min, rtt)
-        # rtt_max = max(rtt_max, rtt)
+        rttCnt+=1
+        rttSum+=rtt
+        rttMin=min(rttMin, rtt)
+        rttMax=max(rttMax, rtt)
 
         ip_header = struct.unpack('!BBHHHBBH4s4s' , recPacket[:20])
         ttl = ip_header[5]
         saddr = inet_ntoa(ip_header[8])
         length = len(recPacket) - 20
 
-        return 'Reply from {}: bytes={} time={:.3f}ms TTL={}'.format(saddr, length, rtt, ttl)
+        return 'Reply from {}: bytes={} RTT={:.3f}ms TTL={}'.format(saddr, length, rtt, ttl)
         #Fill in end
 
         timeLeft = timeLeft - howLongInSelect
@@ -85,7 +84,7 @@ def sendOnePing(mySocket, destAddr, ID):
 	header = struct.pack("bbHHh", ICMP_ECHO_REQUEST, 0, myChecksum, ID, 1)
 	data = struct.pack("d", time.time())
 	# Calculate the checksum on the data and the dummy header.
-	myChecksum = checksum((header + data))
+	myChecksum = checksum(header + data)
 	
 	# Get the right checksum, and put in the header
 	if sys.platform == 'darwin':
@@ -101,7 +100,6 @@ def sendOnePing(mySocket, destAddr, ID):
 	# Both LISTS and TUPLES consist of a number of objects
 	# which can be referenced by their position number within the object.
 
-
 def doOnePing(destAddr, timeout): 
 	icmp = getprotobyname("icmp")
 	# SOCK_RAW is a powerful socket type. For more details:   http://sock-raw.org/papers/sock_raw
@@ -116,27 +114,29 @@ def doOnePing(destAddr, timeout):
 	return delay
 	
 def ping(host, timeout=1):
-    #global rtt_min, rtt_max, rtt_sum, rtt_cnt
-    rtt_min = 1
-    rtt_max = 0
-    rtt_sum = 0
-    rtt_cnt = 0
-    cnt = 0
-    #timeout=1 means: If one second goes by without a reply from the server,
-    #the client assumes that either the client's ping or the server's pong is lost
-    dest = gethostbyname(host)
-    print ("Pinging " + dest + " using Python:")
-    #Send ping requests to a server separated by approximately one second
-    try:
-        while True:
-            cnt += 1
-            print (doOnePing(dest, timeout))
-            time.sleep(1)
-    except KeyboardInterrupt:
-        if cnt != 0:
-            print ('--- {} ping statistics ---'.format(host))
-            print ('{} packets transmitted, {} packets received, {:.1f}% packet loss'.format(cnt, rtt_cnt, 100.0 - rtt_cnt * 100.0 / cnt))
-            if rtt_cnt != 0:
-                print ('round-trip min/avg/max {:.3f}/{:.3f}/{:.3f} ms'.format(rtt_min, rtt_sum / rtt_cnt, rtt_max))
+	global rttMin, rttMax, rttSum, rttCnt
+	rttMin = float('+inf')
+	rttMax = float('-inf')
+	rttSum = 0
+	rttCnt = 0#전역변수초기화
+	count = 0
+	#timeout=1 means: If one second goes by without a reply from the server,
+	#the client assumes that either the client's ping or the server's pong is lost
+	dest=gethostbyname(host)
+	print("Pinging " + dest + " using Python:")
+	#Send ping requests to a server separated by approximately one second
+	try:
+		while 1:
+			count += 1
+			delay=doOnePing(dest, timeout)
+			print(delay)
+			time.sleep(1)
+	except KeyboardInterrupt:
+		if count != 0:
+			print ("***",host," Ping Result ***")
+			print ('{:.1f}% Packet Loss'.format(100.0 - rttCnt * 100.0 / count))
+			if rttCnt != 0:
+				print ('RTT MIN = {:.3f}ms AVG = {:.3f}ms MAX = {:.3f}ms'.format(rttMin, rttSum / rttCnt, rttMax))
 
+	return delay
 ping(sys.argv[1])
